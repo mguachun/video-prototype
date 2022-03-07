074e41ca24fe5ea8d71ed43119d2640096bb5531
Melissa Guachun <1212malka@gmail.com>
2022-03-07 09:55:45 -0500

    testing divs

---
+++ src/home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './home.css';
+
+function Home() {
+
+    return (
+     <div className="homePage">
+         Testing with Different Divs
+         <div className="title">
+             Testing with Different Divs
+            <div className="text">
+                Testing with Different Divs
+            </div>
+        </div>
+     </div>
+    )
+}
+
+export default Home
